2e42b1af657c3e5daa4652a25d10d74a029f95b4
Johany Camirand <johany.camirand@usherbrooke.ca>
2019-09-01 16:13:48 -0400

    creation du projet

---
+++ ConsoleAppClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ConsoleAppClient
+{
+    class Program
+    {
+
+        static HttpClient client = new HttpClient();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            HttpResponseMessage response = client.GetAsync(new Uri("https://localhost:44315/api/values")).Result;
+
+            string responseBody = response.Content.ReadAsStringAsync().Result;
+
+            Console.WriteLine(responseBody);
+        }
+
+    }
+}

f788a7a14596a1f0c7b6b5967b4cf23c784b1286
Johany Camirand <johany.camirand@usherbrooke.ca>
2019-09-01 23:57:14 -0400

    .

--- ConsoleAppClient/Program.cs
+++ ConsoleAppClient/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -11,15 +13,82 @@ namespace ConsoleAppClient
 
         static HttpClient client = new HttpClient();
 
+        static List<Survey> objs = new List<Survey>();
+
+        static Survey selectedSurvey = new Survey();
+
+        static List<string> answers = new List<string>();
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
-            HttpResponseMessage response = client.GetAsync(new Uri("https://localhost:44315/api/values")).Result;
+            GetSurveys();
+        }
+
+        static void GetSurveys()
+        {
+            HttpResponseMessage response = client.GetAsync(new Uri("https://localhost:44315/api/survey")).Result;
 
             string responseBody = response.Content.ReadAsStringAsync().Result;
 
-            Console.WriteLine(responseBody);
+            objs = JsonConvert.DeserializeObject<List<Survey>>(responseBody);
+
+            Console.WriteLine("huehuehue");
+
+            PrintSurveys();
+
+            SelectSurvey();
+
+            AnswerQuestions();
+
+        }
+
+        static void PrintSurveys()
+        {
+            foreach (Survey s in objs)
+            {
+                Console.WriteLine(s.id + " " + s.name);
+            }
+        }
+
+        static void SelectSurvey()
+        {
+            Console.WriteLine("Enter survey id to start");
+
+            string entree = Console.ReadLine();
+
+            selectedSurvey = objs.Find(p => p.id.ToString() == entree);
+
+        }
+
+        static void AnswerQuestions()
+        {
+            foreach (Question q in selectedSurvey.questions)
+            {
+                Console.WriteLine(q.statement);
+                foreach (KeyValuePair<string, string> entry in q.choices)
+                {
+                    Console.WriteLine(entry.Key + ": " + entry.Value);
+                }
+                GetUserAnswer(q.choices);
+            }
+        }
+
+        static void GetUserAnswer(Dictionary<string, string> choices)
+        {
+            string entry = Console.ReadLine();
+            entry = entry.ToLower();
+            if (!choices.ContainsKey(entry))
+            {
+                do
+                {
+                    Console.WriteLine("choix invalide");
+                    entry = Console.ReadLine();
+                    entry = entry.ToLower();
+                } while (!choices.ContainsKey(entry));
+            }
+            answers.Add(entry);
         }
 
     }
---
+++ ConsoleAppClient/Question.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppClient
+{
+    public class Question
+    {
+        public int id { get; set; }
+        public int surveyId { get; set; }
+        public string statement { get; set; }
+        public Dictionary<string, string> choices { get; set; }
+    }
+}

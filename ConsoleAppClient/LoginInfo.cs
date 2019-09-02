667dc05c736157d42cc542abecc92d6e82c8bd64
MarieMichelle <marie-michelle.pepin@usherbrooke.ca>
2019-09-02 11:37:40 -0400

    mini factoring

---
+++ ConsoleAppClient/LoginInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppClient
+{
+    public class LoginInfo
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+    }
+}
--- ConsoleAppClient/Program.cs
+++ ConsoleAppClient/Program.cs
@@ -6,53 +6,22 @@ using System.Threading.Tasks;
 
 namespace ConsoleAppClient
 {
-    public class LoginInfo
-    {
-        public int Id { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
 
-    }
 
     class Program
     {
 
         static HttpClient client = new HttpClient();
-
-        static async Task<Uri> CreateLogin(LoginInfo product)
-        {
-            HttpResponseMessage response = await client.PostAsJsonAsync(
-                "api/login", product);
-            //response.EnsureSuccessStatusCode();
-            //response = client.PostAsJsonAsync(new Uri(responseUrl), userReponseQuestion1).Result;
-            // return URI of the created resource.
-            return response.Headers.Location;
-        }
-
-        static async Task<LoginInfo> GetLoginAsync(string path)
-        {
-            LoginInfo product = null;
-            HttpResponseMessage response = await client.GetAsync(path);
-            if (response.IsSuccessStatusCode)
-            {
-                product = await response.Content.ReadAsAsync<LoginInfo>();
-            }
-            return product;
-        }
-
-        static void ShowLoginInfo(LoginInfo product)
-        {
-            Console.WriteLine($"Name: {product.Username}\tPrice: " +
-                $"{product.Password}");
-        }
-
+        static LoginInfo login = new LoginInfo();
 
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            userLogin();
+        }
 
-            string baseUrl = "https://localhost:44315/";
-
+        static void userLogin()
+        {
             Console.WriteLine("Enter your username:");
 
             string username = Console.ReadLine();
@@ -62,21 +31,12 @@ namespace ConsoleAppClient
             string password = Console.ReadLine();
 
             // Create a new product
-            LoginInfo product = new LoginInfo
-            {
-                Username = username,
-                Password = password
-            };
+            login.Username = username;
+            login.Password = password;
 
-            //var url = CreateLogin(product);
-
-
-            // Get the product
-            //product = GetLoginAsync(url.PathAndQuery);
-            //ShowLoginInfo(product);
             string loginUrl = "https://localhost:44315/api/login";
 
-            HttpResponseMessage response2 = client.PostAsJsonAsync(new Uri(loginUrl), product).Result;
+            HttpResponseMessage response2 = client.PostAsJsonAsync(new Uri(loginUrl), login).Result;
 
             HttpResponseMessage response = client.GetAsync(new Uri(loginUrl)).Result;
 
